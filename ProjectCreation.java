27bac05d2bf045927094dc1c419b64e727678de9
Martin Bozhanski <113229189+MartinBozhanski@users.noreply.github.com>
2022-09-11 17:21:15 +0300

    Add files via upload

---
+++ ProjectCreation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProjectCreation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int projects = Integer.parseInt(scanner.nextLine());
+
+        int neededTime = projects * 3;
+
+        System.out.printf("The architect " + name + " will need " + neededTime +  " hours to complete " + projects + " project/s. ");
+
+
+    }
+}
